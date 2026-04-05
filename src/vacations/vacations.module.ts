4cc04fb9d35da943508973c17ff3a008ae906ac3
Matheus Pessoa <matheus41.dev@gmail.com>
2026-04-05 16:47:55 -0300

    refactor: import TypeOrmModule Vacation and Employee in vacations module

--- src/vacations/vacations.module.ts
+++ src/vacations/vacations.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VacationsService } from './vacations.service';
 import { VacationsController } from './vacations.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Vacation } from './entities/vacation.entity';
+import { Employee } from 'src/employees/entities/employee.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Vacation, Employee])],
   controllers: [VacationsController],
   providers: [VacationsService],
 })
