cfd521c4ef6e76bfce3807fc48b6f04f4c2ddcaf
Matheus Pessoa <matheus41.dev@gmail.com>
2026-04-07 11:46:04 -0300

    feat: import TypOrmModule for Request and Employee entity

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RequestsService } from './requests.service';
 import { RequestsController } from './requests.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Employee } from 'src/employees/entities/employee.entity';
+import { Request } from './entities/request.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Request, Employee])],
   controllers: [RequestsController],
   providers: [RequestsService],
 })
