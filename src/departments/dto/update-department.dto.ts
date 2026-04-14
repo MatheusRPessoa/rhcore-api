df17428c9fbfe4e528d5610f27a8c311fa580c41
matheusdev41 <matheus41.dev@gmail.com>
2026-04-13 21:26:37 -0300

    fix: move STATUS field from create to update DTOs

--- src/departments/dto/update-department.dto.ts
+++ src/departments/dto/update-department.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDepartmentDto } from './create-department.dto';
+import { BaseEntityStatusEnum } from 'src/common/enums/base-entity-status.enum';
+import { IsEnum, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
-export class UpdateDepartmentDto extends PartialType(CreateDepartmentDto) {}
+export class UpdateDepartmentDto extends PartialType(CreateDepartmentDto) {
+  @ApiPropertyOptional({ example: BaseEntityStatusEnum.ATIVO })
+  @IsEnum(BaseEntityStatusEnum, {
+    message: 'O status deve ser um valor válido',
+  })
+  @IsOptional()
+  STATUS?: BaseEntityStatusEnum;
+}
--- src/employees/dto/update-employee.dto.ts
+++ src/employees/dto/update-employee.dto.ts
@@ -1,4 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEmployeeDto } from './create-employee.dto';
+import { BaseEntityStatusEnum } from 'src/common/enums/base-entity-status.enum';
+import { IsEnum, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
-export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {}
+export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {
+  @ApiPropertyOptional({ enum: BaseEntityStatusEnum })
+  @IsEnum(BaseEntityStatusEnum, { message: 'Status inválido' })
+  @IsOptional()
+  STATUS?: BaseEntityStatusEnum;
+}
