8282435bb27afb7c2b08ff2f0409f1bf5ae4030a
Matheus Pessoa <matheus41.dev@gmail.com>
2026-04-15 09:58:06 -0300

    chore: cORS config

--- src/main.ts
+++ src/main.ts
@@ -97,7 +97,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:3000'], // porta do frontend Next.js
+    origin: ['http://localhost:3000', process.env.FRONTEND_URL].filter(Boolean),
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
